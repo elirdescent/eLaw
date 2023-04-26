898e8794cd6343a3dfdf2ed7687e1d7af6b704ad
GentMaloku <gm57330@ubt-uni.net>
2023-04-26 21:54:23 +0200

    added some link

--- eLaw/resources/views/lawyerprofile.blade.php
+++ eLaw/resources/views/lawyerprofile.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/images/elaw.png">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
     <title>Profile</title>
      @vite('resources/css/app.css')
 </head>
