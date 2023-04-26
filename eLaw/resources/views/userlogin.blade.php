b420c739577f3379689de4ef134285b8c43bf5af
GentMaloku <gm57330@ubt-uni.net>
2023-04-26 22:46:49 +0200

    added password input

--- eLaw/resources/views/userlogin.blade.php
+++ eLaw/resources/views/userlogin.blade.php
@@ -26,7 +26,29 @@
                           <span class="block sm:inline">{{$message}} </span>
                         </div>
                         @enderror
-                        
+                        <div  class="flex -mx-3">
+                            <div class="w-full px-3 mb-5">
+                                <label for="" class="text-xs font-semibold px-1">Username</label>
+                                <div class="flex">
+                                    <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                                    <input type="text" class="text-blue-300 w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-500" placeholder="johnsmith" name="username" value="{{old('username')}}" >
+                                </div>
+                            </div> 
+                        </div>
+                        @error('password') 
+                        <div class="bg-red-200 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                          <span class="block sm:inline">{{$message}} </span>
+                        </div>
+                        @enderror
+                        <div class="flex -mx-3">
+                            <div class="w-full px-3 mb-12">
+                                <label for="" class="text-xs font-semibold px-1">Password</label>
+                                <div class="flex">
+                                    <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
+                                    <input type="password" class="text-blue-300 w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-500" placeholder="************" name="password">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
