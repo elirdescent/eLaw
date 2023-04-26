d7ddd00d65e7db9eb410f91f9a89938a372dfa01
GentMaloku <gm57330@ubt-uni.net>
2023-04-26 21:52:12 +0200

    added images

---
+++ eLaw/resources/views/lawyerprofile.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/images/elaw.png">
+    <title>Profile</title>
+     @vite('resources/css/app.css')
+</head>
+<body>
+    
+</body>
+</html>
