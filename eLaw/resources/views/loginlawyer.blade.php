5d1a7aa9e14fb2a3116f1abb87aedbea9e33999f
Deadea1 <tahiridea@outlook.com>
2023-05-02 14:17:18 +0200

    Changed title name

--- eLaw/resources/views/loginlawyer.blade.php
+++ eLaw/resources/views/loginlawyer.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <link rel="icon" type="image/x-icon" href="/images/elaw.png">
-    <title>Document</title>
+    <title>Lawyer Login</title>
    
     
     
