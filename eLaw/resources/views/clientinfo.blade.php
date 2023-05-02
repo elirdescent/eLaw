a9b45320ca9837bd940d7c81fe882b8273ef257e
Deadea1 <tahiridea@outlook.com>
2023-05-02 15:01:56 +0200

    Added navbar to clientinfo page

--- eLaw/resources/views/clientinfo.blade.php
+++ eLaw/resources/views/clientinfo.blade.php
@@ -5,8 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-</head>
-<body>
+   
     
-</body>
-</html>
+    
+    
+@vite('resources/css/app.css')
+    
+</head>
+
+<body>
+    <div class="">
+
+     <!--NAVBAR-->
+
+    <nav class="relative px-4 py-4 flex justify-center items-center bg-gray-700 transition duration-300 ease-in-out">
+
+        <a class="text-3xl font-bold leading-none" href="{{route('homepage')}}">
+          <img src="{{URL('images/elaw.png')}}" class="h-10 " alt="">
+       </a>
+       <a class="text-3xl font-bold leading-none" href="{{route('homepage')}}">
+         <img src="{{URL('images/legalboxteal.png')}}" class="h-10 ml-1 " alt="">
+      </a>    
+      </ul>
+     
+    </nav>
+  <!--NAVBAR-->
