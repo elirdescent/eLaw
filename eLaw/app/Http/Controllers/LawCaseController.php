1c370ea643f69ee1ed9fa92737bd8bc151beb7c3
elirdescent <elshanielird@gmail.com>
2023-05-10 23:07:09 +0200

    Added back-end functionality to add a Legal Case

--- eLaw/app/Http/Controllers/LawCaseController.php
+++ eLaw/app/Http/Controllers/LawCaseController.php
@@ -20,5 +20,38 @@ class LawCaseController extends Controller
        
     }
 
-    
+    public function addCase(Request $request)
+    {
+        $request->validate([
+            
+            'title'=>'required',
+            'name'=>'required',
+            'surname'=>'required',
+            'case_summary'=>'required',
+            'case_description'=>'required',
+            'case_progress'=>'required',
+           
+        ],
+        );
+
+            $case = new LawCase();
+            $case->lawyer_id = session('loginId');
+            $case->title = $request->title;
+            $case->name = $request->name;
+            $case->surname = $request->surname;
+            $case->case_summary = $request->case_summary;
+            $case->case_description = $request->case_description;
+            $case->case_progress = $request->case_progress;
+            $res = $case->save();
+            if($res)
+            {
+                return redirect('dash');
+
+            }
+            else{
+
+                return redirect('home');
+
+            }
+    }
 }
