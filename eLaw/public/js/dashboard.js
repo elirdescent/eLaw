75731f1a825688926a20b5f076cf834235e088f8
Deadea1 <tahiridea@outlook.com>
2023-05-10 22:06:36 +0200

    Added javascript file for dashboard functionality

---
+++ eLaw/public/js/dashboard.js
@@ -0,0 +1,14 @@
+
+const setup = () => {
+    return {
+    isSidebarOpen: false,
+    currentSidebarTab: null,
+    isSettingsPanelOpen: false,
+    isSubHeaderOpen: false,
+    watchScreen() {
+        if (window.innerWidth <= 1024) {
+        this.isSidebarOpen = false
+        }
+    },
+}
+}
