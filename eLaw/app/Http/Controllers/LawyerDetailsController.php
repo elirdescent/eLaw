d57c7a122f40cdfe6477eb5b45d5ef54f8360106
GentMaloku <gm57330@ubt-uni.net>
2023-05-24 15:41:12 +0200

    added Create Profile Function

--- eLaw/app/Http/Controllers/LawyerDetailsController.php
+++ eLaw/app/Http/Controllers/LawyerDetailsController.php
@@ -28,7 +28,61 @@ class LawyerDetailsController extends Controller
            
     }
 
-   
+    public function createProfile(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|regex:/^[a-zA-Z]+$/',
+            'surname'=>'required|regex:/^[a-zA-Z]+$/',
+            'username'=>'required|regex:/^[a-zA-Z]+$/',
+            'city'=>'required|regex:/^[a-zA-Z]+$/',
+            'country'=>'required|regex:/^[a-zA-Z]+$/',
+            'job'=>'required',
+            'pricing'=>'required',
+            'bio'=>'required'
+
+        ],
+    [
+            'name.regex' => 'The :attribute field may only contain letters.',
+            'name.not_regex' => 'The :attribute field may not contain numbers.', 
+            'surname.regex' => 'The :attribute field may only contain letters.', 
+            'surname.not_regex' => 'The :attribute field may not contain numbers.', 
+            'username.regex' => 'The :attribute field may only contain letters.', 
+            'username.not_regex' => 'The :attribute field may not contain numbers.',
+            'city.regex' => 'The :attribute field may only contain letters.', 
+            'city.not_regex' => 'The :attribute field may not contain numbers.', 
+            'country.regex' => 'The :attribute field may only contain letters.', 
+            'country.not_regex' => 'The :attribute field may not contain numbers.', 
+    ]);
+
+    $details = new LawyerDetails();
+    $details->lawyer_id = session('loginId');
+    $details->name = $request->name;
+    $details->surname = $request->surname;
+    $details->username = $request->username;
+    $details->city = $request->city;
+    $details->country = $request->country;
+    $details->address = $request->address;
+    $details->job = $request->job;
+    $details->pricing = $request->pricing;
+    $details->bio = $request->bio;
+    $res = $details->save();
+    if($res)
+    {
+
+         
+
+        return back()->with('success','Profile created successfully!');
+    }
+    else
+    {
+        return back()->with('error','Profile already exists or could not be created!');
+    }
+
+    
+    }
+
+
+    
     
 
 }
