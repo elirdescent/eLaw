5bafd6dd365842e1f50fababf5cd3200d2f0a882
Deadea1 <tahiridea@outlook.com>
2023-05-24 15:22:14 +0200

    added addTask function for task

--- eLaw/app/Http/Controllers/TaskController.php
+++ eLaw/app/Http/Controllers/TaskController.php
@@ -19,5 +19,35 @@ class TaskController extends Controller
         
        
     }
+    public function addTask(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+            'name'=>'required',
+            'surname'=>'required',
+            'task_description'=>'required',
+            'case_progress'=>'required'
+        ]
+            
+        );
+
+        $task = new Task();
+        $task->lawyer_id = session('loginId');
+        $task->title = $request->title;
+        $task->name = $request->name;
+        $task->surname = $request->surname;
+        $task->task_description = $request->task_description;
+        $task->case_progress = $request->case_progress;
+        $res = $task->save();
+        if($res)
+        {
+            return back()->with('success','Task added successfully!');
+        }
+        else
+        {
+            return back()->with('fail','Case could not be added!');
+        }
+        }
+  
    
 }
