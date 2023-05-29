fe1c3679649db13a61d5130820fbfee39d652d2b
GentMaloku <gm57330@ubt-uni.net>
2023-05-29 16:15:38 +0200

    Added ReviewController

---
+++ eLaw/app/Http/Controllers/ReviewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\Review;
+use App\Models\Client;
+
+class ReviewController extends Controller
+{
+   
+}
