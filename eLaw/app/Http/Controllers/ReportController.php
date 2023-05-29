46c15227030dc967552bd2549df92df4fc2c82e9
GentMaloku <gm57330@ubt-uni.net>
2023-05-29 16:10:01 +0200

    added store function to ReportController

--- eLaw/app/Http/Controllers/ReportController.php
+++ eLaw/app/Http/Controllers/ReportController.php
@@ -7,7 +7,25 @@ use App\Models\Report;
 
 class ReportController extends Controller
 {
-    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'description'=>'required'
+        ]);
+
+        $report = new Report();
+        $report->description = $request->description;
+        $res = $report->save();
+        if($res)
+        {
+            return back()->with('success','Report posted successfully!');
+        }
+        else
+        {
+            return back()->with('fail','Report could not be posted!');
+        }
+
+    }
 
     
 }
